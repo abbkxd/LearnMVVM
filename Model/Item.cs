691583a676d9197165e43789e124c1ca84b6d8cd
abbkxd <168463890+abbkxd@users.noreply.github.com>
2025-01-16 18:32:11 +0000

    Add project files.

---
+++ Model/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnMVVM.Model
+{
+    internal class Item
+    {
+        public String Name { get; set; }
+        public String SerialNumber { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,45 @@
+﻿using LearnMVVM.Model;
+using LearnMVVM.MVVM;
+using System.Collections.ObjectModel;
+
+namespace LearnMVVM.ViewModel
+{
+    internal class MainWindowViewModel:ViewModelBase
+    {
+        public ObservableCollection<Item> Items { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Items = new ObservableCollection<Item>();
+            Items.Add(new Item() {
+                Name = "Item1",
+                SerialNumber="S001",
+                Quantity=1
+            });
+            Items.Add(new Item()
+            {
+                Name = "Item2",
+                SerialNumber = "S002",
+                Quantity = 4
+            });
+            Items.Add(new Item()
+            {
+                Name = "Item3",
+                SerialNumber = "S003",
+                Quantity = 6
+            });
+        }
+
+        private Item selectedItem;
+
+        public Item SelectedItem
+        {
+            get { return selectedItem; }
+            set { selectedItem = value;
+                OnPropertyChanged();
+                    }
+        }
+
+
+    }
+}
